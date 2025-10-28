1787162a7c022016f19224cb6e54e7ffefabfc8c
Jafar Mohamed <jafarmohamed955@gmail.com>
2025-10-28 06:38:30 +0300

    Add files via upload
    simple counter project

---
+++ project5/script.js
@@ -0,0 +1,8 @@
+var count = 0;
+
+function Increase(){
+    document.getElementById('count').innerHTML = count+=1;
+}
+function Decrease(){
+    document.getElementById('count').innerHTML = count-=1;
+}
